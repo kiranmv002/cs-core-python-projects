e32a614c33b71a4bfcd64711932d58016e59b247
M V Kiran <mada.kiran@s.amity.edu>
2026-03-05 17:45:09 +0530

    Update deadlock_detection.py

--- dbms/deadlock_detection.py
+++ dbms/deadlock_detection.py
@@ -1,1 +1,38 @@
+# Deadlock Detection Algorithm
+# Operating Systems - Day 19
 
+
+def deadlock_detection(processes, resources, allocation, request, available):
+    
+    finish = [False] * processes
+
+    # mark processes with no allocation as finished
+    for i in range(processes):
+        if sum(allocation[i]) == 0:
+            finish[i] = True
+
+    work = available.copy()
+
+    changed = True
+
+    while changed:
+        changed = False
+
+        for i in range(processes):
+            if not finish[i]:
+                
+                possible = True
+
+                for j in range(resources):
+                    if request[i][j] > work[j]:
+                        possible = False
+                        break
+
+                if possible:
+                    for j in range(resources):
+                        work[j] += allocation[i][j]
+
+                    finish[i] = True
+                    changed = True
+
+    deadlocked = []
