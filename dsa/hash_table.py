a6f81d6d262c87647399f022d1a1cce2f4cde741
M V Kiran <mada.kiran@s.amity.edu>
2026-05-05 22:07:27 +0530

    Update hash_table.py

--- dsa/hash_table.py
+++ dsa/hash_table.py
@@ -35,3 +35,59 @@ class HashTable:
 
         print("Key not found")
 
+    # Delete
+    def delete(self, key):
+        index = self.hash_function(key)
+
+        for i, pair in enumerate(self.table[index]):
+            if pair[0] == key:
+                self.table[index].pop(i)
+                print("Key deleted")
+                return
+
+        print("Key not found")
+
+    # Display
+    def display(self):
+        print("\nHash Table:")
+        for i, bucket in enumerate(self.table):
+            print(i, ":", bucket)
+
+
+# -------- MAIN --------
+
+size = int(input("Enter size of hash table: "))
+ht = HashTable(size)
+
+while True:
+    print("\n--- Hash Table Menu ---")
+    print("1. Insert")
+    print("2. Search")
+    print("3. Delete")
+    print("4. Display")
+    print("5. Exit")
+
+    ch = input("Enter choice: ")
+
+    if ch == "1":
+        key = int(input("Enter key (number): "))
+        value = input("Enter value: ")
+        ht.insert(key, value)
+
+    elif ch == "2":
+        key = int(input("Enter key to search: "))
+        ht.search(key)
+
+    elif ch == "3":
+        key = int(input("Enter key to delete: "))
+        ht.delete(key)
+
+    elif ch == "4":
+        ht.display()
+
+    elif ch == "5":
+        print("Exiting...")
+        break
+
+    else:
+        print("Invalid choice!")
