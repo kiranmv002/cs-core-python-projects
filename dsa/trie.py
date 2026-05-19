dcd860d1bdb616e6fca0ae39cfb1d70a87dd179b
M V Kiran <mada.kiran@s.amity.edu>
2026-05-19 22:15:09 +0530

    Update trie.py

--- dsa/trie.py
+++ dsa/trie.py
@@ -57,3 +57,41 @@ class Trie:
 
         for ch in node.children:
             self.display(node.children[ch], word + ch)
+
+
+# -------- MAIN --------
+
+trie = Trie()
+
+while True:
+
+    print("\n--- Trie Menu ---")
+    print("1. Insert Word")
+    print("2. Search Word")
+    print("3. Display Words")
+    print("4. Exit")
+
+    choice = input("Enter choice: ")
+
+    if choice == "1":
+
+        word = input("Enter word: ")
+        trie.insert(word)
+
+    elif choice == "2":
+
+        word = input("Enter word to search: ")
+        trie.search(word)
+
+    elif choice == "3":
+
+        print("\nStored Words:")
+        trie.display()
+
+    elif choice == "4":
+
+        print("Exiting...")
+        break
+
+    else:
+        print("Invalid choice!")
